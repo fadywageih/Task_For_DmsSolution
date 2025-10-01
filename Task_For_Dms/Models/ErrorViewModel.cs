c7572a777e66febdd60624272c7a05e2ba89e83e
fadywageih <fadywageih14@gmail.com>
2025-10-01 05:17:55 +0300

    Add project files.

---
+++ Task_For_Dms/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Task_For_Dms.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
